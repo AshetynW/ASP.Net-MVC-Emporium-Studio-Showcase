6bcb610800c0f95cafb73c18f09e497287bdf07f
ashetyn white <ashetynw@gmail.com>
2019-04-09 15:43:15 -0400

    commit added Char Creator framework

---
+++ ASP Web Proto/Controllers/DemoAppsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_Web_Proto.Controllers
+{
+    public class DemoAppsController : Controller
+    {
+        // GET: DemoApps
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult RPGCharacterCreator()
+        {
+            ViewBag.Message = "RPG Character Creator";
+            return View();
+        }
+    }
+}
