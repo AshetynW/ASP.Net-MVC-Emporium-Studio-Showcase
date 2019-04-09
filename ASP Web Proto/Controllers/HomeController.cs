f18f605799aa7859cbd030eb1e955b97c1e21261
Ashetyn White <32204279+AshetynW@users.noreply.github.com>
2019-04-09 11:57:19 -0400

    Main app Folder contents initially committed

---
+++ ASP Web Proto/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_Web_Proto.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About Emporium.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult DemoApps()
+        {
+            ViewBag.Message = "Apps";
+            return View();
+        }
+    }
+}
---
+++ ASP Web Proto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_Web_Proto.Startup))]
+namespace ASP_Web_Proto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
